1ccc24eb6330a3b84290c9ef69a4684c9cb7ee5e
dragoon125 <santiprime2@gmail.com>
2025-04-04 23:22:12 -0400

    Update test_ assignment_3.py

--- test/test_ assignment_3.py
+++ test/test_ assignment_3.py
@@ -1,1 +1,12 @@
-
+def gaus():
+    a = np.array([[2, -1, 1], [1, 3, 1], [-1, 5, 4]])
+    return a
+def lu():
+    a = np.array([[1, 1, 0, 3], [2, 1, -1, 1], [3, -1, -1, 2], [-1, 2, 3, -1]])
+    return a
+def diagonally():
+    a = np.array([[9, 0, 5, 2, 1], [3, 9, 1, 2, 1], [0, 1, 7, 2, 3], [4, 2, 3, 12, 2], [3, 2, 4, 0, 8]])
+    return a
+def positive():
+    a = np.array([[2, 2, 1], [2, 3, 0], [1, 0, 2]])
+    return a
